f0115fc38b8167e49f327b0b842345ebbfee75b7
Frederik Fix <ich@derfred.com>
2012-01-14 22:42:31 +0100

    config rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,43 @@
 require 'rspec'
 $LOAD_PATH << File.expand_path("../lib", File.dirname(__FILE__))
 require 'resque-pubsub'
+
+RSpec.configure do |config|
+  config.before :each do
+    Resque.redis.flushall
+  end
+end
+
+#
+# Test Workers
+#
+
+class BillingListener
+
+  @queue = :billing
+
+  def self.perform(user_hash)
+    raise "failure" if user_hash[:name] == "failure"
+  end
+
+end
+
+class AccountListener
+
+  @queue = :account
+
+  def self.perform(user_hash)
+    
+  end
+
+end
+
+class NewUserListener
+
+  @queue = :new_user
+
+  def self.perform(user_hash)
+    
+  end
+
+end
